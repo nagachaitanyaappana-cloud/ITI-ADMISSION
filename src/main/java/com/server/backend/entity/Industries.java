5ca749007eeaf63fa5d3e80eee6dc92b0eb94885
bhavanibrahmadevarapu <bhavanibrahmadevarapu@gmail.com>
2026-08-25 10:08:20 +0530

    Added industry entity

---
+++ src/main/java/com/server/backend/entity/Industries.java
@@ -0,0 +1,61 @@
+package com.server.backend.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import jakarta.persistence.Column;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+@Entity
+@Table(name="industries" ,schema="placements")
+@Data
+public class Industries {
+    @Id
+    @Column(name="slno")
+    private BigInteger slno;
+
+    @Column(name="dist_code")
+     private Integer distCode;
+
+     @Column(name="dist_name")
+     private String distName;
+
+     @Column(name="industry_id")
+     private BigInteger industryId;
+
+     @Column(name="industry_name")
+     private String industryName;
+
+     @Column(name="industry_type")
+     private String industryType;
+
+     @Column(name="iti_code")
+     private Integer itiCode;
+
+     @Column(name="iti_name")
+     private String itiName;
+
+     @Column(name="ncvt_mis_code")
+     private String ncvtMisCode;
+
+     @Column(name="no_of_trades")
+     private Integer noOfTrades;
+
+     @Column(name="no_of_units")
+     private String noOfUnits;
+
+     @Column(name="trade_code")
+     private Integer tradeCode;
+
+     @Column(name="trade_name")
+     private String tradeName;
+
+     @Column(name="entry_by")
+     private String entryBy;
+
+     @Column(name="entry_time")
+     private Timestamp entryTime;
+
+     @Column(name="trade_short")
+    private String tradeShort;
+}
---
+++ src/main/java/com/server/backend/entity/IndustryMaster.java
@@ -0,0 +1,37 @@
+package com.server.backend.entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import jakarta.persistence.Column;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+@Entity
+@Table(name="industry_master" ,schema="placements")
+@Data
+public class IndustryMaster {
+    @Id
+    @Column(name="industry_id")
+    private BigInteger industryId;
+
+    @Column(name="industry_address")
+    private String industryAddress;
+
+    @Column(name="industry_name")
+    private String industryName;
+
+    @Column(name="industry_type")
+    private String industryType;
+
+    @Column(name="entry_by")
+    private String entryBy;
+
+    @Column(name="entry_time")
+    private Timestamp entryTime;
+
+    @Column(name="edit_by")
+    private String editBy;
+    
+    @Column(name="edit_time")
+    private Timestamp editTime;
+}
