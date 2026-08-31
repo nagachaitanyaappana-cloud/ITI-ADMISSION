4e6017d95a276120e0176333b33cf1ef5851f86c
bhavanibrahmadevarapu <bhavanibrahmadevarapu@gmail.com>
2026-08-31 15:35:36 +0530

    v

--- src/main/java/com/server/backend/Repository/PlacementsRepositories/ImplantRepository.java
+++
@@ -1,8 +0,0 @@
-package com.server.backend.Repository.PlacementsRepositories;
-import com.server.backend.entity.Placements.ImplantEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-   
-
-public interface ImplantRepository  extends JpaRepository<ImplantEntity, Long> {
-}
-
