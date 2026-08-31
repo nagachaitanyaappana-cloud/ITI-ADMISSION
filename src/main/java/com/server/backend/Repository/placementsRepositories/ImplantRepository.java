823d7a7b90ea947fea85a16244db3b22aa1920d9
bhavanibrahmadevarapu <bhavanibrahmadevarapu@gmail.com>
2026-08-31 15:07:33 +0530

    d

--- src/main/java/com/server/backend/Repository/placementsRepositories/ImplantRepository.java
+++ src/main/java/com/server/backend/Repository/placementsRepositories/ImplantRepository.java
@@ -2,8 +2,7 @@ package com.server.backend.Repository.PlacementsRepositories;
 import com.server.backend.entity.Placements.ImplantEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
    
-import java.util.List;
+
 public interface ImplantRepository  extends JpaRepository<ImplantEntity, Long> {
-    List<ImplantEntity> findByItiCode(Integer itiCode);
 }
 
